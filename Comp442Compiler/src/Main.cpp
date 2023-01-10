b2f40cc43b2323da44cbe65d9cd1893037a20bb5
Aether700 <hanachian.jamil@gmail.com>
2023-01-10 09:57:53 -0500

    modified the code so it can also work on vscode with current local setup

--- Comp442Compiler/src/Main.cpp
+++ Comp442Compiler/src/Main.cpp
@@ -1,14 +1,25 @@
 #include <iostream>
 #include "Lexer/Lexer.h"
 
-int main()
+int main(int argc, char* argv[])
 {
-	std::ifstream file = std::ifstream("testInputFile.txt");
+	std::string fileName = "testInputFile.txt";
+	std::ifstream file = std::ifstream(fileName);
 	if (!file)
 	{
-		std::cout << "cannot find file\n";
-		return 1;
+		// extract path for vscode
+		std::string currDir = std::string(argv[0]);
+		std::string path = currDir.substr(0, currDir.find_last_of("/\\"));
+		path = path.substr(0, path.find_last_of("/\\"));
+		path += "/Comp442Compiler/Comp442Compiler/" + fileName;
+		
+		file = std::ifstream(path);
+		if (!file)
+		{
+			std::cout << "cannot find file\n";
+			return 1;
+		}
 	}
 	Token t = Lexer::GetNextToken(file);
-	int x = 0;
+	std::cout << "token lexeme: " << t.GetLexeme() << "\n";
 }
