e8c0268c918603c0581fb106c85f8572c0f400f3
Aether700 <hanachian.jamil@gmail.com>
2023-02-04 12:17:02 -0500

    Started implementing the framework necessary for the parser

--- Comp442Compiler/src/Parser/Parser.cpp
+++ Comp442Compiler/src/Parser/Parser.cpp
@@ -1,6 +1,110 @@
 #include "Parser.h"
 #include "../Core/Token.h"
 #include "../Lexer/Lexer.h"
+#include "../Core/Core.h"
+
+// StackableItem //////////////////////////////////////
+StackableItem::StackableItem(TokenType t) : m_isTerminal(true), m_item(t) { }
+StackableItem::StackableItem(NonTerminal nonTerminal) 
+    : m_isTerminal(false), m_item(nonTerminal) { }
+
+StackableItem::~StackableItem()
+{
+    if (IsTerminal())
+    {
+        m_item.m_terminal.~TokenType();
+    }
+    else
+    {
+        m_item.m_nonTerminal.~NonTerminal();
+    }
+}
+    
+bool StackableItem::IsTerminal() const { return m_isTerminal; }
+
+TokenType StackableItem::GetTerminal() const 
+{
+    if (!IsTerminal())
+    {
+        DEBUG_BREAK();
+        return TokenType::None;
+    }
+    return m_item.m_terminal;
+}
+
+NonTerminal StackableItem::GetNonTerminal() const
+{
+    if (IsTerminal())
+    {
+        DEBUG_BREAK();
+        return NonTerminal::None;
+    }
+    return m_item.m_nonTerminal;
+}
+
+StackableItem::Item::Item(TokenType t) : m_terminal(t) { }
+StackableItem::Item::Item(NonTerminal nonTerminal) : m_nonTerminal(nonTerminal) { }
+StackableItem::Item::~Item() { }
+
+// Rule //////////////////////////////////////////////
+
+Rule::Rule(NonTerminal left, const std::initializer_list<StackableItem>& right) 
+    : m_leftSide(left), m_rightSide(right) { }
+    
+NonTerminal Rule::GetLeftSide() const { return m_leftSide; }
+const std::list<StackableItem>& Rule::GetRightSide() const { return m_rightSide; }
+
+// RuleManager /////////////////////////////////////////////////////////////////////////
+const Rule* RuleManager::GetRule(RuleID id)
+{
+    if (id == NullRule)
+    {
+        DEBUG_BREAK();
+        return nullptr;
+    }
+    return GetInstance().m_rules[id];
+}
+
+RuleManager::RuleManager() { InitializeRules(); }
+RuleManager::~RuleManager() 
+{ 
+    for (Rule* r : m_rules)
+    {
+        delete r;
+    }
+}
+
+RuleManager& RuleManager::GetInstance()
+{
+    static RuleManager manager;
+    return manager;
+}
+
+void RuleManager::InitializeRules()
+{
+    // temp rules for testing
+    m_rules[0] = new Rule(NonTerminal::Start, { TokenType::And, NonTerminal::Start });
+}
+
+// ParsingTableEntry ///////////////////////////////////////////////////////////////////
+ParsingTableEntry::ParsingTableEntry(
+    const std::initializer_list<std::pair<TokenType, RuleID>>& entries)
+{
+    for (auto& pair : entries)
+    {
+        m_entries[pair.first] = pair.second;
+    }
+}
+
+RuleID ParsingTableEntry::GetRule(const Token& t) const
+{
+    auto it = m_entries.find(t.GetTokenType());
+    if (it == m_entries.end())
+    {
+        return NullRule;
+    }
+    return it->second;
+}
 
 // Parser ////////////////////////////////////////////
 void Parser::Parse(const std::string& filepath)
@@ -10,10 +114,24 @@ void Parser::Parse(const std::string& filepath)
     // Begin parsing here
 }
 
-Parser::Parser() { }
+Parser::Parser() { InitializeParsingTable(); }
+
+Parser::~Parser()
+{
+    for (auto& pair : m_parsingTable)
+    {
+        delete pair.second;
+    }
+}
 
 Parser& Parser::GetInstance()
 {
     static Parser p;
     return p;
+}
+
+void Parser::InitializeParsingTable()
+{
+    // temp table for testing
+    m_parsingTable[NonTerminal::Start] = new ParsingTableEntry({{TokenType::And, 0}});
 }
--- Comp442Compiler/src/Parser/Parser.h
+++ Comp442Compiler/src/Parser/Parser.h
@@ -1,5 +1,82 @@
 #pragma once
 #include <string>
+#include <unordered_map>
+#include <vector>
+#include <list>
+
+#include "../Core/Token.h"
+
+typedef size_t RuleID;
+static constexpr RuleID NullRule = SIZE_MAX;
+
+enum class NonTerminal
+{
+    None,
+    Start
+};
+
+class StackableItem
+{
+public:
+    StackableItem(TokenType t);
+    StackableItem(NonTerminal nonTerminal);
+    ~StackableItem();
+    
+    bool IsTerminal() const;
+    TokenType GetTerminal() const;
+    NonTerminal GetNonTerminal() const;
+
+private:
+    union Item
+    {
+        Item(TokenType t);
+        Item(NonTerminal nonTerminal);
+        ~Item();
+
+        TokenType m_terminal;
+        NonTerminal m_nonTerminal;
+    } m_item;
+    bool m_isTerminal;
+};
+
+class Rule
+{
+public:
+    Rule(NonTerminal left, const std::initializer_list<StackableItem>& right);
+    
+    NonTerminal GetLeftSide() const;
+    const std::list<StackableItem>& GetRightSide() const;
+
+private:
+    NonTerminal m_leftSide;
+    std::list<StackableItem> m_rightSide;
+};
+
+class RuleManager
+{
+public:
+    static const Rule* GetRule(RuleID id);
+
+private:
+    RuleManager();
+    ~RuleManager();
+
+    static RuleManager& GetInstance();
+    void InitializeRules();
+
+    std::vector<Rule*> m_rules;
+};
+
+class ParsingTableEntry
+{
+public:
+    ParsingTableEntry(const std::initializer_list<std::pair<TokenType, RuleID>>& entries);
+
+    // returns NullRule if the provided token does not have a valid entry in the table
+    RuleID GetRule(const Token& t) const;
+private:
+    std::unordered_map<TokenType, RuleID> m_entries;
+};
 
 class Parser
 {
@@ -7,5 +84,10 @@ public:
     static void Parse(const std::string& filepath);
 private:
     Parser();
+    ~Parser();
     static Parser& GetInstance();
+
+    void InitializeParsingTable();
+
+    std::unordered_map<NonTerminal, ParsingTableEntry*> m_parsingTable;
 };
