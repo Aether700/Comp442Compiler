d9f0242e130f6beddadf3a5fb1db6655a2fe3f7f
Aether700 <hanachian.jamil@gmail.com>
2023-02-19 11:45:03 -0500

    Created additional Node types

--- Comp442Compiler/src/Parser/AST.cpp
+++ Comp442Compiler/src/Parser/AST.cpp
@@ -24,3 +24,56 @@ void ASTNode::AddChild(ASTNode* child)
 
 auto ASTNode::begin() { return m_children.begin(); }
 auto ASTNode::end() { return m_children.end(); }
+
+// VarNode //////////////////////////////////////////////////
+VarNode::VarNode(const std::string& id) : m_id(id) { }
+VarNode::VarNode(const std::string& id, const std::list<std::string>& indices) 
+    : m_id(id), m_indices(indices) { }
+
+const std::string& VarNode::GetID() const { return m_id; }
+const std::list<std::string>& VarNode::GetIndices() const { return m_indices; }
+
+// VarDeclNode ////////////////////////////////////////////
+VarDeclNode::VarDeclNode(const std::string& id, const std::string& type) 
+    : VarNode(id), m_type(type) { }
+
+VarDeclNode::VarDeclNode(const std::string& id, const std::string& type, 
+    const std::list<std::string>& arraySize) : VarNode(id, arraySize), m_type(type) { }
+
+const std::string& VarDeclNode::GetType() const { return m_type; }
+const std::list<std::string>& VarDeclNode::GetArraySize() const { return GetIndices(); }
+
+// DotNode ////////////////////////////////////////////////////////
+DotNode::DotNode(ASTNode* leftSide, ASTNode* rightSide)
+{
+    AddChild(leftSide);
+    AddChild(rightSide);
+}
+
+ASTNode* DotNode::GetLeft()
+{
+    return *begin();
+}
+
+ASTNode* DotNode::GetRight()
+{
+    return *(begin()++);
+}
+
+
+// ProgramNode /////////////////////////////////////
+ProgramNode::ProgramNode()
+{
+    AddChild(new ClassListNode());
+    AddChild(new FreeFunctionListNode());
+}
+
+ClassListNode* ProgramNode::GetClassList()
+{
+    return (ClassListNode*)*begin();
+}
+
+FreeFunctionListNode* ProgramNode::GetFunctionList()
+{
+    return (FreeFunctionListNode*)*(begin()++);
+}
--- Comp442Compiler/src/Parser/AST.h
+++ Comp442Compiler/src/Parser/AST.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <list>
+#include <string>
 
 class ASTNode
 {
@@ -18,4 +19,70 @@ public:
 private:
     ASTNode* m_parent;
     std::list<ASTNode*> m_children;
+};
+
+class VarNode : public ASTNode
+{
+public:
+    VarNode(const std::string& id);
+    VarNode(const std::string& id, const std::list<std::string>& indices);
+
+    const std::string& GetID() const;
+    const std::list<std::string>& GetIndices() const;
+
+private:
+    std::string m_id;
+    std::list<std::string> m_indices;
+};
+
+class VarDeclNode : public VarNode
+{
+public:
+    VarDeclNode(const std::string& id, const std::string& type);
+    VarDeclNode(const std::string& id, const std::string& type, 
+        const std::list<std::string>& arraySize);
+
+    const std::string& GetType() const;
+    const std::list<std::string>& GetArraySize() const;
+
+private:
+    std::string m_type;
+};
+
+class DotNode : public ASTNode
+{
+public:
+    DotNode(ASTNode* leftSide, ASTNode* rightSide);
+
+    ASTNode* GetLeft();
+    ASTNode* GetRight();
+};
+
+class ClassDefNode : public ASTNode
+{
+    // Not implemented yet
+};
+
+class FreeFunctionDefNode : public ASTNode
+{
+    // Not implemented yet
+};
+
+class ClassListNode : public ASTNode
+{
+    // Not implemented yet
+};
+
+class FreeFunctionListNode : public ASTNode
+{
+    // Not implemented yet
+};
+
+class ProgramNode : public ASTNode
+{
+public:
+    ProgramNode();
+
+    ClassListNode* GetClassList();
+    FreeFunctionListNode* GetFunctionList();
 };
